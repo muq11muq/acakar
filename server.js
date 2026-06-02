9d80e9106d86ed5a1cade34af1ff2d3850cfafbe
zbxhhhhhh <zbxhhhhhh@gmail.com>
2026-06-03 01:50:20 +0300

    Configure Railway for Node.js static server

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(filePath);
+  
+  const contentType = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'application/javascript',
+    '.json': 'application/json'
+  }[ext] || 'text/plain';
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
+});
+
+server.listen(process.env.PORT || 8000, '0.0.0.0', () => {
+  console.log('Server running on port ' + (process.env.PORT || 8000));
+});
