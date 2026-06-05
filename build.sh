1192c0f59993bbb89c2764f95aa25402dc163ae3
zbxhhhhhh <zbxhhhhhh@gmail.com>
2026-06-05 18:21:28 +0300

    Add mkdir and --clear to build.sh for static files

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ export PYTHONPATH=/app
 export MISE_PYTHON_GITHUB_ATTESTATIONS=false
 mise install
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+mkdir -p staticfiles
+python manage.py collectstatic --noinput --clear
 python manage.py migrate --noinput
